6ca3a4d2d0ab9f94c8ec8bf61626abc7fa407a73
how-chen <howard0528@gmail.com>
2017-02-28 14:10:11 -0600

    Create rotX.m

---
+++ kinematics/rotX.m
@@ -0,0 +1,7 @@
+function [out]=RotX(theta)
+  % Author: Howard Chen
+  % 3x3 Rotation Matrix around X-axis (right-handed) 
+  % See: https://en.wikipedia.org/wiki/Rotation_matrix
+  
+  out=[1,0,0;0,cos(theta),-sin(theta);0,sin(theta),cos(theta)];
+end
