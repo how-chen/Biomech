b45638392a594d68ef7b8319201824115a907d61
how-chen <howard0528@gmail.com>
2017-03-01 13:46:51 -0600

    Update quatZ.m

--- kinematics/quatZ.m
+++ kinematics/quatZ.m
@@ -1,10 +1,11 @@
 function [ quat ] = quatZ( theta )
-%QUATX Summary of this function goes here
-%   Detailed explanation goes here
+%QUATX right-handed rotation around z-axis in quaternions
+%   quat: n x 4 array (1st column is real) 
+%   Author: Howard Chen
+%   Affiliation: University of Iowa
+
 quat(:,1)=cos(theta./2);
 quat(:,2)=zeros(length(theta),1);
 quat(:,3)=zeros(length(theta),1);
 quat(:,4)=sin(theta./2);
-
-
 end
