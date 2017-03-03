1f385e28fc5d74fca64ff71e8fef677590f7bf5f
how-chen <howard0528@gmail.com>
2017-03-03 00:47:46 -0600

    Add files via upload
    Removed intermediary quaternion operations

--- IMU/Spatial Orientation/accelMag.m
+++ IMU/Spatial Orientation/accelMag.m
@@ -1,44 +1,26 @@
-function [ q ] = accelMag( accel,mag )
-%ACCELMAG calculates orientation using accel & mag
-%   calculations is done in ypr and converted to quaternions
+function [ q ] = accelMag( accel,mag)
+%ACCELMAG calculates orientation using accelerometer & magnetometers
+%   calculations is done in yaw-pitch-roll and converted to quaternions
+%   accel: nx3 vector with accelerometer measurements (gravity vector)
+%   mag: nx3 vector with magnetometer measurements (magetic north vector) 
+%
+%   Author: Howard Chen
+%   Date Created: 3/2/2017
 
-n=length(accel);
+% calculate pitch and roll from accel
 
-%% calculate pitch and roll from accel
 %pitch
-pitch=atan2(-accel(:,1),sqrt(accel(:,2).^2+accel(:,3).^2));
+pitch = atan2(-accel(:,1),sqrt(accel(:,2).^2+accel(:,3).^2));
 
 %roll
-roll=atan2(accel(:,2),accel(:,3));
+roll = atan2(accel(:,2),accel(:,3));
 
-%% rotate mag to earth coordinate frame
-% (quaternion rotation is used to avoid 'for' loop)
+% calculate yaw from mag and accel
+yaw=atan2(mag(:,3).*sin(roll)-mag(:,2).*cos(roll),mag(:,1).*cos(pitch)+mag(:,2).*sin(pitch).*sin(roll)+mag(:,3).*sin(pitch).*cos(roll));
 
-%construct attitude quaternion
-qAtt(:,1)=cos(pitch./2).*cos(roll./2);
-qAtt(:,2)=cos(pitch./2).*sin(roll./2);
-qAtt(:,3)=sin(pitch./2).*cos(roll./2);
-qAtt(:,4)=-sin(pitch./2).*sin(roll./2);
-
-%rotate mag to earth frame
-M=qM(qAtt,qM([zeros(n,1),mag],[qAtt(:,1),-qAtt(:,2:4)]));
-M(:,1)=[];
-
-%% calculate yaw
-yaw=atan2(-M(:,2),M(:,1));
-
-%% convert ypr to quaternion
-q(:,1)=cos(yaw./2).*cos(pitch./2).*cos(roll./2)+sin(yaw./2).*sin(pitch./2).*sin(roll./2);
-q(:,2)=cos(yaw./2).*cos(pitch./2).*sin(roll./2)-sin(yaw./2).*sin(pitch./2).*cos(roll./2);
-q(:,3)=cos(yaw./2).*sin(pitch./2).*cos(roll./2)+sin(yaw./2).*cos(pitch./2).*sin(roll./2);
-q(:,4)=sin(yaw./2).*cos(pitch./2).*cos(roll./2)-cos(yaw./2).*sin(pitch./2).*sin(roll./2);
+% convert ypr to quaternion
+q(:,1) = cos(yaw./2).*cos(pitch./2).*cos(roll./2)+sin(yaw./2).*sin(pitch./2).*sin(roll./2);
+q(:,2) = cos(yaw./2).*cos(pitch./2).*sin(roll./2)-sin(yaw./2).*sin(pitch./2).*cos(roll./2);
+q(:,3) = cos(yaw./2).*sin(pitch./2).*cos(roll./2)+sin(yaw./2).*cos(pitch./2).*sin(roll./2);
+q(:,4) = sin(yaw./2).*cos(pitch./2).*cos(roll./2)-cos(yaw./2).*sin(pitch./2).*sin(roll./2);
 end
-
-%% quaternion multiplication function
-function [ qOut ] = qM( q1, q2 )
-qOut=[q1(:,1).*q2(:,1)-q1(:,2).*q2(:,2)-q1(:,3).*q2(:,3)-q1(:,4).*q2(:,4),... 
-      q1(:,1).*q2(:,2)+q1(:,2).*q2(:,1)+q1(:,3).*q2(:,4)-q1(:,4).*q2(:,3),...
-      q1(:,1).*q2(:,3)-q1(:,2).*q2(:,4)+q1(:,3).*q2(:,1)+q1(:,4).*q2(:,2),...
-      q1(:,1).*q2(:,4)+q1(:,2).*q2(:,3)-q1(:,3).*q2(:,2)+q1(:,4).*q2(:,1)]; 
-end
-
