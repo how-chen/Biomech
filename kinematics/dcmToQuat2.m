493a2bfd6ffd5cdef825d32b7a55b63d4fdf4702
how-chen <howard0528@gmail.com>
2017-03-23 15:06:48 -0500

    Add files via upload

--- kinematics/dcmToQuat2.m
+++ kinematics/dcmToQuat2.m
@@ -1,41 +1,52 @@
 function [ quat ] = dcmToQuat2( R )
-%UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
+% dcmToQuat2  will convert a dcm to a quaternion (q1 is real)
+% This function will calculate quaternion from DCM using 
+% four different methods for each sample. The most stable method
+% (largest normalization factor in denominator) will be used.
+%
+% input: R (3x3xn)
+% ouput: quat (nx4) column 1 is real
+% Author: Howard Chen
+% Date: 3/20/16
+% Update: 3/17/16
 
-q = zeros(4,1);
-quat=zeros(1,4);
+sz = size(R);
+ln = sz(3); 
 
-q(4) = 1/2*sqrt(1+R(1,1)+R(2,2)+R(3,3));
-q(1) = 1/2*sqrt(1+R(1,1)-R(2,2)-R(3,3));
-q(2) = 1/2*sqrt(1-R(1,1)+R(2,2)-R(3,3));
-q(3) = 1/2*sqrt(1-R(1,1)-R(2,2)+R(3,3));
+quat=zeros(ln,4);
 
-[k, in] = max(q);
+for i=1:ln
 
-if in == 1
-    q(2) = 1/4/q(1)*(R(1,2)+R(2,1));
-    q(3) = 1/4/q(1)*(R(1,3)+R(3,1));
-    q(4) = 1/4/q(1)*(R(3,2)-R(2,3));
-end
-if in == 2
-    q(1) = 1/4/q(2)*(R(1,2)+R(2,1));
-    q(3) = 1/4/q(2)*(R(2,3)+R(3,2));
-    q(4) = 1/4/q(2)*(R(1,3)-R(3,1));
-end
-if in == 3
-    q(1) = 1/4/q(3)*(R(1,3)+R(3,1));
-    q(2) = 1/4/q(3)*(R(2,3)+R(3,2));
-    q(4) = 1/4/q(3)*(R(2,1)-R(1,2));
-end
-if in == 4
-    q(1) = 1/4/q(4)*(R(3,2)-R(2,3));
-    q(2) = 1/4/q(4)*(R(1,3)-R(3,1));
-    q(3) = 1/4/q(4)*(R(2,1)-R(1,2));
-end
+    quat(i,1) = 1/2*sqrt(1+R(1,1,i)+R(2,2,i)+R(3,3,i));
+    quat(i,2) = 1/2*sqrt(1+R(1,1,i)-R(2,2,i)-R(3,3,i));
+    quat(i,3) = 1/2*sqrt(1-R(1,1,i)+R(2,2,i)-R(3,3,i));
+    quat(i,4) = 1/2*sqrt(1-R(1,1,i)-R(2,2,i)+R(3,3,i));
 
-quat(1)=q(4);
-quat(2:4)=q(1:3);
+    [~, in] = max(quat(i,:));
 
+    if in == 2
+        quat(i,3) = 1/4/quat(i,2)*(R(1,2,i)+R(2,1,i));
+        quat(i,4) = 1/4/quat(i,2)*(R(1,3,i)+R(3,1,i));
+        quat(i,1) = 1/4/quat(i,2)*(R(3,2,i)-R(2,3,i));
+    end
+
+    if in == 3
+        quat(i,2) = 1/4/quat(i,3)*(R(1,2,i)+R(2,1,i));
+        quat(i,4) = 1/4/quat(i,3)*(R(2,3,i)+R(3,2,i));
+        quat(i,1) = 1/4/quat(i,3)*(R(1,3,i)-R(3,1,i));
+    end
+
+    if in == 4
+        quat(i,2) = 1/4/quat(i,4)*(R(1,3,i)+R(3,1,i));
+        quat(i,3) = 1/4/quat(i,4)*(R(2,3,i)+R(3,2,i));
+        quat(i,1) = 1/4/quat(i,4)*(R(2,1,i)-R(1,2,i));
+    end
+
+    if in == 1
+        quat(i,2) = 1/4/quat(i,1)*(R(3,2,i)-R(2,3,i));
+        quat(i,3) = 1/4/quat(i,1)*(R(1,3,i)-R(3,1,i));
+        quat(i,4) = 1/4/quat(i,1)*(R(2,1,i)-R(1,2,i));
+    end
 
 end
 
