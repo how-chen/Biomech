4baebe5888b2f8d049846a66e1a8d4b35eeb6f37
how-chen <howard0528@gmail.com>
2017-05-16 12:54:23 -0500

    Add files via upload

--- sensors/importActigraph.m
+++ sensors/importActigraph.m
@@ -2,9 +2,6 @@ function [ data ] = importActigraph(PathName,FileName)
 %ACTIGRAPHTOIMU Imports Actigraph Link Files 
 %Author: Howard Chen
 %Date: 3-29-17
-% data(:,1:3) = accelerometer (m/s^2)
-% data(:,4:6) = gyroscope (rad/s)
-% data(:,7:9) = magnetometer (uT)
 
 datafile=csvread(strcat(PathName,FileName),11,1);
 
@@ -15,7 +12,7 @@ data(:,1:3)=datafile(:,1:3).*9.81;
 data(:,4:6)=degtorad(datafile(:,5:7));
 
 %Magnetometer (May need to re-order it)
-data(:,7:9)=datafile(:,7:9);
+data(:,7:9)=datafile(:,8:10);
    
 end
 
